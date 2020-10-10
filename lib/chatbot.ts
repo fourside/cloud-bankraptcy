1bf210d9b2dd279e8b986a7dbdcc67ad4590c0cb
fourside <fourside@gmail.com>
2020-10-10 12:30:25 +0900

    chatbot logging

--- lib/chatbot.ts
+++ lib/chatbot.ts
@@ -1,6 +1,8 @@
 import { Construct } from "@aws-cdk/core";
-import { SlackChannelConfiguration } from "@aws-cdk/aws-chatbot";
+import { SlackChannelConfiguration, LoggingLevel } from "@aws-cdk/aws-chatbot";
 import { Topic } from "@aws-cdk/aws-sns";
+import { PolicyStatement, Effect, ServicePrincipal, Role, PolicyDocument } from "@aws-cdk/aws-iam";
+import { RetentionDays } from "@aws-cdk/aws-logs";
 
 export function createChatbot(
   scope: Construct,
@@ -9,10 +11,28 @@ export function createChatbot(
   channelId: string,
   topic: Topic
 ): void {
-  const channelConfig = new SlackChannelConfiguration(scope, `${name}-chatbot`, {
+  const role = new Role(scope, `${name}-chatbot-role`, {
+    assumedBy: new ServicePrincipal("chatbot.amazonaws.com"),
+    inlinePolicies: {
+      cloudwatchAccess: new PolicyDocument({
+        statements: [
+          new PolicyStatement({
+            effect: Effect.ALLOW,
+            resources: ["*"],
+            actions: ["cloudwatch:Describe*", "cloudwatch:Get*", "cloudwatch:List*"],
+          }),
+        ],
+      }),
+    },
+  });
+
+  new SlackChannelConfiguration(scope, `${name}-chatbot`, {
     slackChannelConfigurationName: name,
     slackWorkspaceId: workspaceId,
     slackChannelId: channelId,
-    notificationTopics:[topic],
+    notificationTopics: [topic],
+    logRetention: RetentionDays.ONE_MONTH,
+    loggingLevel: LoggingLevel.ERROR,
+    role: role,
   });
 }
